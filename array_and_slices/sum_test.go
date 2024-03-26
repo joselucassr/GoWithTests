a8c82cf419706a5450411d3ca95d2cf2bcd20976
Lucas <joselucasrs@gmail.com>
2024-03-26 15:26:52 -0300

    refactor to use slices

--- array_and_slices/sum_test.go
+++ array_and_slices/sum_test.go
@@ -3,10 +3,27 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of five numbers", func(t *testing.T) {
 
-	got := Sum(numbers)
-	expected := 15
+		numbers := []int{1, 2, 3, 4, 5}
+
+		got := Sum(numbers)
+		expected := 15
+		compareResults(t, expected, got, numbers)
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		expected := 6
+		compareResults(t, expected, got, numbers)
+	})
+}
+
+func compareResults(t testing.TB, expected, got int, numbers []int) {
+	t.Helper()
 
 	if expected != got {
 		t.Errorf("expected %d but got %d, given %v", expected, got, numbers)
