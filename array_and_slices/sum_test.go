7f08e600ecf7f25fb4c999ea27236aa5bdcb9b18
Lucas <joselucasrs@gmail.com>
2024-03-26 15:42:58 -0300

    basic SumAll

--- array_and_slices/sum_test.go
+++ array_and_slices/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of five numbers", func(t *testing.T) {
@@ -9,7 +12,7 @@ func TestSum(t *testing.T) {
 
 		got := Sum(numbers)
 		expected := 15
-		compareResults(t, expected, got, numbers)
+		compareSum(t, expected, got, numbers)
 	})
 
 	t.Run("collection of any size", func(t *testing.T) {
@@ -18,14 +21,23 @@ func TestSum(t *testing.T) {
 
 		got := Sum(numbers)
 		expected := 6
-		compareResults(t, expected, got, numbers)
+		compareSum(t, expected, got, numbers)
 	})
 }
 
-func compareResults(t testing.TB, expected, got int, numbers []int) {
+func compareSum(t testing.TB, expected, got int, numbers []int) {
 	t.Helper()
 
 	if expected != got {
 		t.Errorf("expected %d but got %d, given %v", expected, got, numbers)
+	}
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2, 3}, []int{1, 2, 3, 4, 5})
+	expected := []int{6, 15}
+
+	if !slices.Equal(expected, got) {
+		t.Errorf("expected %v but got %v", expected, got)
 	}
 }
