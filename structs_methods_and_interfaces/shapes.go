d8767fcf64bf21b06a5cf3cec6aed4bebb90b87c
Lucas <joselucasrs@gmail.com>
2024-03-27 15:48:09 -0300

    added triangle

--- structs_methods_and_interfaces/shapes.go
+++ structs_methods_and_interfaces/shapes.go
@@ -26,3 +26,12 @@ type Circle struct {
 func (c Circle) Area() float64 {
 	return math.Pi * math.Pow(c.Radius, 2)
 }
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
