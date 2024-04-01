b7b113542f3bfe150695026b799bb18d06b062b7
Zé <joselucasrs@gmail.com>
2024-03-31 21:45:11 -0300

    implementing Stringer

--- pointers_and_errors/wallet.go
+++ pointers_and_errors/wallet.go
@@ -2,15 +2,25 @@ package pointersanderrors
 
 import "fmt"
 
-type Wallet struct {
-	balance int
+type Bitcoin int
+
+type Stringer interface {
+	String() string
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Printf("adress of balance in test is %p \n", &w.balance)
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
--- pointers_and_errors/wallet_test.go
+++ pointers_and_errors/wallet_test.go
@@ -8,14 +8,14 @@ import (
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
 
-	wallet.Deposit(10)
+	wallet.Deposit(Bitcoin(10))
 
 	got := wallet.Balance()
-	expected := 10
+	expected := Bitcoin(10)
 
 	fmt.Printf("adress of balance in test is %p \n", &wallet.balance)
 
 	if expected != got {
-		t.Errorf("expected %d but got %d", expected, got)
+		t.Errorf("expected %s but got %s", expected, got)
 	}
 }
