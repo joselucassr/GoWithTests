1f0d5f13482b7b40f69a896bf57b4283a47bdd86
Zé <joselucasrs@gmail.com>
2024-03-31 22:05:40 -0300

    checking for error text

--- pointers_and_errors/wallet_test.go
+++ pointers_and_errors/wallet_test.go
@@ -15,11 +15,15 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
-	assertError := func(t testing.TB, err error) {
+	assertError := func(t testing.TB, got error, expected string) {
 		t.Helper()
 
-		if err == nil {
-			t.Error("wanted an error but didn't get one")
+		if got == nil {
+			t.Fatal("wanted an error but didn't get one")
+		}
+
+		if got.Error() != expected {
+			t.Errorf("got %q, expected %q", got, expected)
 		}
 	}
 
@@ -46,7 +50,7 @@ func TestWallet(t *testing.T) {
 		wallet := Wallet{startingBalance}
 		err := wallet.Withdraw(Bitcoin(100))
 
-		assertError(t, err)
+		assertError(t, err, "cannot withdraw, insufficient funds")
 		assertBalance(t, wallet, startingBalance)
 	})
 }
