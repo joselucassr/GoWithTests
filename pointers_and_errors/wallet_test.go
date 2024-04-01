071cc32c995d7e5345b01ed95d2956bc0e83232d
Zé <joselucasrs@gmail.com>
2024-03-31 21:53:41 -0300

    test refactor

--- pointers_and_errors/wallet_test.go
+++ pointers_and_errors/wallet_test.go
@@ -1,24 +1,27 @@
 package pointersanderrors
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
+	assertBalance := func(t testing.TB, wallet Wallet, expected Bitcoin) {
+		t.Helper()
+
+		got := wallet.Balance()
+
+		if expected != got {
+			t.Errorf("expected %s but got %s", expected, got)
+		}
+	}
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
 		wallet.Deposit(Bitcoin(10))
 
-		got := wallet.Balance()
 		expected := Bitcoin(10)
-
-		fmt.Printf("adress of balance in test is %p \n", &wallet.balance)
-
-		if expected != got {
-			t.Errorf("expected %s but got %s", expected, got)
-		}
+		assertBalance(t, wallet, expected)
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
@@ -26,11 +29,7 @@ func TestWallet(t *testing.T) {
 
 		wallet.Withdraw(Bitcoin(10))
 
-		got := wallet.Balance()
 		expected := Bitcoin(10)
-
-		if expected != got {
-			t.Errorf("expected %s but got %s", expected, got)
-		}
+		assertBalance(t, wallet, expected)
 	})
 }
