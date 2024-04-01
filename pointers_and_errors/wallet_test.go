4400fa0ce6b5f8f1b6fcde72da3e7ed101409e4e
Zé <joselucasrs@gmail.com>
2024-03-31 21:49:26 -0300

    implementing Withdraw

--- pointers_and_errors/wallet_test.go
+++ pointers_and_errors/wallet_test.go
@@ -6,16 +6,31 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
 
-	wallet.Deposit(Bitcoin(10))
+		wallet.Deposit(Bitcoin(10))
 
-	got := wallet.Balance()
-	expected := Bitcoin(10)
+		got := wallet.Balance()
+		expected := Bitcoin(10)
 
-	fmt.Printf("adress of balance in test is %p \n", &wallet.balance)
+		fmt.Printf("adress of balance in test is %p \n", &wallet.balance)
 
-	if expected != got {
-		t.Errorf("expected %s but got %s", expected, got)
-	}
+		if expected != got {
+			t.Errorf("expected %s but got %s", expected, got)
+		}
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+		expected := Bitcoin(10)
+
+		if expected != got {
+			t.Errorf("expected %s but got %s", expected, got)
+		}
+	})
 }
